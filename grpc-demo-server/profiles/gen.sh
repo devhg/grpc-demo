a6154b8b6b1f138114d506cb75402215d0819b74
QXQZX <501966782@qq.com>
2020-10-04 20:54:37 +0800

    support github.com/envoyproxy/protoc-gen-validate

--- grpc-demo-server/profiles/gen.sh
+++ grpc-demo-server/profiles/gen.sh
@@ -1,5 +1,5 @@
 protoc --go_out=plugins=grpc:../ prod.proto
-protoc --go_out=plugins=grpc:../ models.proto
+protoc --go_out=plugins=grpc:../ --validate_out=lang=go:../ models.proto
 protoc --go_out=plugins=grpc:../ order.proto
 protoc --grpc-gateway_out=logtostderr=true:../ prod.proto
 protoc --grpc-gateway_out=logtostderr=true:../ models.proto
--- grpc-demo-server/service/order_service.go
+++ grpc-demo-server/service/order_service.go
@@ -9,6 +9,10 @@ type OrderService struct {
 }
 
 func (o OrderService) NewOrder(ctx context.Context, or *OrderRequest) (*OrderResponse, error) {
+	err := or.OrderMain.Validate()
+	if err != nil {
+		return &OrderResponse{Status: "error", Message: err.Error()}, nil
+	}
 	fmt.Println(or.OrderMain)
 	fmt.Println(or.OrderMain.Details)
 	ret := &OrderResponse{Status: "200", Message: "ok!"}
